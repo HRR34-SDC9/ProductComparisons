6168c1e48c4087b24b5c45f20664c3150933158b
DavidRez <davidraviles@gmail.com>
2018-11-28 12:00:51 -0800

    Knex set up files.

---
+++ seeds/maria.js
@@ -0,0 +1,13 @@
+
+exports.seed = (knex, Promise) => {
+  return knex('tents').truncate()
+    .then(() => {
+      return knex('shirts').truncate();
+    })
+    .then(() => {
+      return knex.raw(`LOAD DATA LOCAL INFILE 'db/tents_maria.csv' INTO TABLE trailblazer.tents fields terminated BY "," lines terminated BY "\n" (imageURL, title, ranking, reviews, price, sleepingCapacity, packagedWeight, numberOfDoors, bestUse, productType)`)
+    })
+    .then(() => {
+      return knex.raw(`LOAD DATA LOCAL INFILE 'db/shirts_maria.csv' INTO TABLE trailblazer.shirts fields terminated BY "," lines terminated BY "\n" (imageURL, title, ranking, reviews, price, productType)`)
+    });
+};
