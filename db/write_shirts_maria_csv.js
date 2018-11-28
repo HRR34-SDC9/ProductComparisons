58622d8e324237b373ac3afa29c26889dac6b1d6
DavidRez <davidraviles@gmail.com>
2018-11-28 12:32:18 -0800

    MariaDB csv writers.

---
+++ db/write_shirts_maria_csv.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const faker = require('faker');
+
+const printer = () => {
+    const file = fs.createWriteStream(__dirname + '/shirts_maria.csv');
+    let i = 0;
+    const MAX_LIM = 5000000;
+
+    const writer = function () {
+        let result = true;
+
+        // Write to file until we get false as result
+        while (i < MAX_LIM && result) {
+            result = file.write(`https://s3-us-west-1.amazonaws.com/trailblazer-images/shirt_${faker.random.number({min:1, max:25})}.jpg,`+
+                                `${faker.commerce.productAdjective() + ' ' + faker.commerce.productMaterial()},`+
+                                `${faker.random.arrayElement([0,0.5,1,1.5,2,2.5,3,3.5,4,4.5,5])},`+
+                                `${faker.random.number({min:0, max: 99})},`+
+                                `${faker.finance.amount(10,65,2)},`+
+                                `Shirt\n`);
+
+            // even if the result is false, our write has been probably
+            // written to the buffer. A false value denotes that the our last
+            // write has resulted in buffered data, crossing the highWaterMark.
+            // So, we have to increment the count.
+            i += 1;
+        }
+        // Add an event listener if the last write was not successful
+        if (i < MAX_LIM)
+        file.once('drain', writer);
+    }
+    return writer;
+  }
+  
+ const print= printer();
+ print();
